ac39255268b97de61d96aec74944fa404065eed3
NolanCampbell2357 <nolanc1223@gmail.com>
2023-03-24 11:07:02 -0500

    Setup navbar for use

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -14,8 +14,8 @@ function App() {
 
   return (
     <div>
-      <NavBar />
       <BrowserRouter>
+      <NavBar />
         <Routes>
           <Route path="/" element={<MonthlyCalendar />} />
         </Routes>
