9180876ade187345898b626abe4ff6f3cd016b6a
zoeysears <zoey.mae.sears@gmail.com>
2023-03-25 08:06:23 -0500

    Loan Estimator Form

--- FrontEnd/src/Components/LoanEstimator.js
+++ FrontEnd/src/Components/LoanEstimator.js
@@ -1,5 +1,37 @@
 import React from "react";
-
+import { Grid, Button, TextField } from "@mui/material";
 export default function () {
-  return <h1>Loan Estimator View</h1>;
+  return  (<div>
+  <form>
+    <Grid container rowSpacing={1}>
+        <h2>Loan Estimator</h2>
+      <Grid item xs={12}>
+        <TextField
+          type="text"
+          variant="outlined"
+          placeholder="Loan Amount"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          type="number"
+          variant="outlined"
+          placeholder="Loan Term"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          type="number"
+          variant="outlined"
+          placeholder="Interest Rate"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </Grid>
+  </form>
+</div>)
 }
