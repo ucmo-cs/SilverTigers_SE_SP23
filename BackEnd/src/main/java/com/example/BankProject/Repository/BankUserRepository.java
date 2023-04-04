6534e52494b0190e80581833232e2adbe0d1c146
NolanCampbell2357 <nolanc1223@gmail.com>
2023-04-03 23:27:11 -0500

    Setup savings goal backend

--- BackEnd/src/main/java/com/example/BankProject/Repository/BankUserRepository.java
+++ BackEnd/src/main/java/com/example/BankProject/Repository/BankUserRepository.java
@@ -3,8 +3,6 @@ package com.example.BankProject.Repository;
 
 import com.example.BankProject.Domain.BankUser;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
