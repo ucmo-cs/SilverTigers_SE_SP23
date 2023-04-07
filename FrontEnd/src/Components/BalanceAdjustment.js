96a97915c260cbd07e01f6e90fa08060dae4d65d
ett642 <74976855+ett642@users.noreply.github.com>
2023-04-07 12:01:40 -0500

    Update BalanceAdjustment.js
    Added the input fields and radio buttons for the balance adjustment

--- FrontEnd/src/Components/BalanceAdjustment.js
+++ FrontEnd/src/Components/BalanceAdjustment.js
@@ -1,6 +1,59 @@
 import React from "react";
 
+import { Grid, Button, TextField } from "@mui/material";
 export default function () {
-    
-  return <h1>Balance Adjustment View</h1>;
+    return (<div>
+        <form>
+            <Grid container rowSpacing={1}>
+                <h2>Balance Estimator</h2>
+                <Grid item xs={12}>
+                    <h3>Current Balance</h3>
+                   </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        type="text"
+                        variant="outlined"
+                        placeholder="Amount"
+                    ></TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        type="number"
+                        variant="outlined"
+                        placeholder="Date"
+                    ></TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        type="number"
+                        variant="outlined"
+                        placeholder="Reason"
+                    ></TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <label>
+                        <input
+                            type="radio"
+                            name="up check"
+                            value="Planned"
+                        />
+                        Planned
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="up check"
+                            value="Unplanned"
+                        />
+                        Unplanned
+                    </label>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" type="submit">
+                        Submit
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>
+    </div>)
 }
