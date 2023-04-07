db147229659216dd46c4c647ddcef5050d3e2e34
zoeysears <zoey.mae.sears@gmail.com>
2023-04-07 15:13:49 -0500

    cleaned up login page

--- FrontEnd/src/Components/Login.js
+++ FrontEnd/src/Components/Login.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import React from "react";
 import PropTypes from "prop-types";
 import { Grid, Button, TextField } from "@mui/material";
+import logo from '../Resources/2018cbcagreenverticalfdic.png';
 Login.propTypes = {
   setUserToken: PropTypes.func.isRequired,
 };
@@ -46,42 +47,53 @@ function Login({ setUserToken }) {
       });
     console.log(user);
   };
+const h2Style = {
+    color: 'green',
+    background: "#f0fff0",
+    textAlign: 'center',
+    width: 1500,
+  };
 
   return (
-    <div>
-      <form onSubmit={processForm}>
-        <Grid container rowSpacing={1}>
-            <h2>Commerce Bank</h2>
-          <Grid item xs={12}>
-            <TextField
-              label="Username"
-              name="username"
-              value={user.username}
-              onChange={changeValue}
-              type="text"
-              variant="outlined"
-              placeholder="Enter Username"
-            ></TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Password"
-              name="password"
-              value={user.password}
-              onChange={changeValue}
-              type="password"
-              variant="outlined"
-              placeholder="Enter password"
-            ></TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </Grid>
+  <div>
+    <img src={logo} height={100} width={405} />
+    <form onSubmit={processForm} style={{background: "#f0fff0", height: '90vh', minHeight: '90vh'}}>
+      <Grid container style={h2Style} rowSpacing={2} sx={{marginTop: 10}}>
+          <h2 style={h2Style}>Login</h2> 
+          <h style={h2Style}>Please enter your username and password.</h> 
+        <Grid item xs={15}>
+          <TextField
+            label="Username"
+            name="username"
+            value={user.username}
+            onChange={changeValue}
+            type="text"
+
+            variant="outlined"
+            placeholder="Enter Username"
+          ></TextField>
         </Grid>
-      </form>
-    </div>
+        <Grid item xs={12}>
+          <TextField
+            label="Password"
+            name="password"
+            value={user.password}
+            onChange={changeValue}
+            type="password"
+            variant="outlined"
+            placeholder="Enter password"
+          ></TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <Button style={{color: 'white', background: 'green',  width: '17.5%'}} variant="contained"  type="submit">
+            Submit
+          </Button>
+          <p style={{color: 'green', background: "#f0fff0", textAlign: 'center', marginBottom: '10%'}}><u>Create Account</u></p>
+        </Grid>
+      </Grid>
+    </form>
+  </div>
+
   );
 }
 
