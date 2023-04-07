a69fd292d740f74fc1b8eadc1b9ca36da90c62ae
ett642 <74976855+ett642@users.noreply.github.com>
2023-04-07 15:06:04 -0500

    Update BalanceAdjustment.js
    Trying to work out how to get BalanceTable.js to display properly on here. Skeleton of the look with no functionality

--- FrontEnd/src/Components/BalanceAdjustment.js
+++ FrontEnd/src/Components/BalanceAdjustment.js
@@ -1,14 +1,103 @@
-import React from "react";
+import { Typography, Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import BalanceTable from "./BalanceTable";
+//import MonthSelect from "./MonthSelect";
+//import SavingsGoal from "./SavingsGoal";
 
 import { Grid, Button, TextField } from "@mui/material";
 export default function () {
-    return (<div>
+
+    const userId = sessionStorage.getItem("userToken");
+
+    const currentBOM = () => {
+        const current = new Date();
+        return new Date(current.getFullYear(), current.getMonth(), 1);
+    };
+
+    const currentEOM = () => {
+        const current = new Date();
+        return new Date(current.getFullYear(), current.getMonth() + 1, 0);
+    };
+
+    const [activity, setActivity] = useState([]);
+    const [currentBalance, setCurrentBalance] = useState(0.0);
+    const [startBalance, setStartBalance] = useState(0.0);
+    const [startDate, setStartDate] = useState(currentBOM());
+    const [endDate, setEndDate] = useState(currentEOM());
+
+    const calculateCurrentBalance = (activity) => {
+        const dateTime = new Date();
+
+        //const currentActivity = activity.filter(
+            //(rec) => rec.filterDate.getTime() <= dateTime.getTime()
+        //);
+
+        setCurrentBalance(
+            currentActivity
+                .map((rec) => rec.amount)
+                .reduce((total, current) => total + current)
+        );
+    };
+
+    /*const calculateStartBalance = (activity) => {
+        const currentActivity = activity.filter(
+            (rec) => rec.filterDate.getTime() < startDate.getTime()
+        );
+
+        const amounts = currentActivity.map((rec) => rec.amount);
+
+        if (amounts.length === 0) {
+            setStartBalance(0.0);
+        } else {
+            setStartBalance(amounts.reduce((total, current) => total + current));
+        }
+    };
+
+    const calculateCurrentMonthStartBalance = (activity) => {
+        const currentActivity = activity.filter(
+            (rec) => rec.filterDate.getTime() < new Date().getTime()
+        );
+
+        const amounts = currentActivity.map((rec) => rec.amount);
+
+        if (amounts.length === 0) {
+            return 0;
+        }
+        return amounts.reduce((total, current) => total + current);
+    };*/
+
+    useEffect(() => {
+        fetch("http://localhost:8080/users/" + userId + "/statements", {
+            method: "GET",
+        })
+            .then((res) => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    return null;
+                }
+            })
+            /*then((res) => {
+                res.forEach((row) => {
+                    row.filterDate = new Date(row.date);
+                });
+                setActivity(res);
+                calculateCurrentBalance(res);
+                calculateStartBalance(res);
+            });*/
+    }, []);
+
+    //useEffect(() => {
+        //calculateStartBalance(activity);
+    //}, [startDate, endDate]);
+
+    return (<><div>
         <form>
             <Grid container rowSpacing={1}>
                 <h2>Balance Estimator</h2>
                 <Grid item xs={12}>
                     <h3>Current Balance</h3>
-                   </Grid>
+                </Grid>
                 <Grid item xs={12}>
                     <TextField
                         type="text"
@@ -27,7 +116,7 @@ export default function () {
                     <TextField
                         type="number"
                         variant="outlined"
-                        placeholder="Reason"
+                        placeholder="Description"
                     ></TextField>
                 </Grid>
                 <Grid item xs={12}>
@@ -35,16 +124,14 @@ export default function () {
                         <input
                             type="radio"
                             name="up check"
-                            value="Planned"
-                        />
+                            value="Planned" />
                         Planned
                     </label>
                     <label>
                         <input
                             type="radio"
                             name="up check"
-                            value="Unplanned"
-                        />
+                            value="Unplanned" />
                         Unplanned
                     </label>
                 </Grid>
@@ -55,5 +142,17 @@ export default function () {
                 </Grid>
             </Grid>
         </form>
-    </div>)
+    </div>
+        <Box sx={{ width: "100%" }}>
+                <Box sx={{ width: "100%", display: "flex", flexDirection: "row" }}>
+                    <BalanceTable
+                        activity={activity}
+                        startBalance={startBalance}
+                        startDate={startDate}
+                        endDate={endDate}
+                        addDel={true}
+                    />
+            </Box>
+        </Box></>   )
+
 }
